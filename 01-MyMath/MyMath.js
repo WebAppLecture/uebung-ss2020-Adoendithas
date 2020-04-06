aa399fa11b9d8350c44b7555286004f8c9f52202
Adoendithas <adrian.algasinger@web.de>
2020-04-06 14:36:32 +0200

    Test Ueb1

--- 01-MyMath/MyMath.js
+++ 01-MyMath/MyMath.js
@@ -18,27 +18,67 @@ export class MyMath {
         this.value = value * 1; 
     }
 
-    add(value) {
+    //Gives an Alert with actual value
+    show() {
+        alert("Value is " + this.value);
+    }
 
+    // Set the value of myMath manually:
+    set(value) {
+        this.value = value;
+        return this;
+    }
+
+    add(value) {
+        this.value += value;
+        return this;
     }
 
     subtract(value) {
-
+        this.value -= value;
+        return this;
     }
 
     multiply(value) {
-
+        this.value *= value;
+        return this;
     }
 
     divide(value) {
+        if (value == 0) {
+            if (confirm("You are about to divide by ZERO, do you know what you`re doing?!?")) {
+                alert("Action canceled to prevent cracks in the space-time continuum!");
+              } else {
 
+              }
+        }
+        else {
+            this.value /= value;
+        }
+
+        return this;
     }
 
     pow(value) {
+        let temp = this.value;
+        for (let a = 2; a <= value; a++) {
+            this.value *= temp;
+        }
 
+
+        return this;
     }
 
     faculty() {
+        if (this.value % 1 == 0) {
+            for (let a = (this.value - 1); a > 1; a--) {
+                this.value *= a;
+            }
+        }
+        else {
+            alert(this.value + " is no valid integer.");
+        }
 
+        return this;
     }
 }
