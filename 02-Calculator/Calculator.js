9067bdb07bab23a606bfcbf0b8e386aa4a782c0b
Adoendithas <adrian.algasinger@web.de>
2020-04-07 12:20:00 +0200

    erste Abgabe

--- 02-Calculator/Calculator.js
+++ 02-Calculator/Calculator.js
@@ -3,14 +3,26 @@ import {MyMath} from "../01-MyMath/MyMath.js";
 export class Calculator {
 
     constructor(numpad, outputCalculation, outputSolution) {
+        this.numPad = numpad;
+        this.outputCalculation = outputCalculation;
+        this.outputSolution = outputSolution;
+        this.setupNumPad();
     }
 
     setupNumPad() { 
-
+        //10 Tasten DOM Event Listener:
+        //for (let i = 0; i<10; i++) {
+        var key;
+        for (key of this.numPad.children) {
+            //key.addEventListener("click", this.onButtonClick.bind(this));
+            key.addEventListener("click",  function() {
+                console.log(this.value);
+              });
+        }
     }
 
     onButtonClick(symbol) {
-    
+        console.log(this.value);
     }
 
     print(string) {
